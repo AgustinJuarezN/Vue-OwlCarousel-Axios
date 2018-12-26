3444824cab3dce1c59cf4aa4af16079db58aaacc
AgustinJuarezN <43082143+AgustinJuarezN@users.noreply.github.com>
2018-12-26 15:46:18 -0300

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <div id="app">
+    <img alt="vue" src="./assets/logo.png" id="logo">
+    <Slide/>
+  </div>
+</template>
+
+<script>
+import Slide from './components/Slide.vue'
+export default {
+  name: 'app',
+  components: {
+    Slide
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #000;
+}
+
+body{
+  background-color: #000;
+}
+
+</style>
